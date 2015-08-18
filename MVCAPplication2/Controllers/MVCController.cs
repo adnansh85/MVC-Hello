69fb4677596cf6b4bca19179e5dc487ed72da143
adnan_shaikh <85.adnan@gmail.com>
2015-08-18 00:52:29 -0400

    first commit

---
+++ MVCAPplication2/Controllers/MVCController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class MVCController : Controller
+    {
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var controller = new MVCController();
+
+            // Act
+            var result = controller.Index() as ContentResult;
+
+           
+            Assert.AreEqual("Hello World!!.", result.Content);
+        }
+
+       
+
+    }
+   
+
+   
+
+}
